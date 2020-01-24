5c91042e4cb34019bd5c791606bead814f5c5323
Stephen Ouellette <stephen.r.ouellette@gmail.com>
2020-01-24 12:50:35 -0700

    Fixed Header

--- Taproot/App.js
+++ Taproot/App.js
@@ -3,9 +3,9 @@ import {
   StyleSheet,
   View } from 'react-native';
 
-  import Header from '../components/Header';
-  import Card from '../components/Card'; // provides card object
-  import Colors from '../constants/Colors'; // provides primary and tertiary color for scheme
+  import Header from './components/Header';
+  import Card from './components/Card'; // provides card object
+  import Colors from './constants/Colors'; // provides primary and tertiary color for scheme
 
 // use "CTRL + /" to add a comment line. Outside of render blocks you can use //
 // and inside render blocks you can use {/* */} for your comment blocks...
@@ -17,15 +17,14 @@ export default function App() {
     // First view is the base view that all apps will need. this is the main view.
     // when adding the style property, you need to use the binding {} brackets
     <View style={styles.view_mainView}>
-        <Header/>
+        <Header title="Taproot"/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   view_mainView: {
-    flex: 1,
-    alignItems: 'center',
+    flex: 1
   }
 });
 
--- Taproot/components/Header.js
+++ Taproot/components/Header.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Image } from 'react-native';
 
 import Colors from '../constants/Colors';
+import Taproot_Logo from '../assets/Taproot_Logo_RGB.jpg';
 
 const Header = props => {
     // this style setup allows us to override styles with our own styles to increase options..
@@ -24,7 +25,7 @@ const styles = StyleSheet.create ({
         alignItems: 'center',
         justifyContent: 'center'
     },
-    logo: {
+    image_logo: {
         resizeMode: 'center',
         marginTop: -280, // trying to get local position correct, this will change...
         marginBottom: -320 // same with the line above (positions the line break)...
