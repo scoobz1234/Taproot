c3bd1864fd02c3b4848f87efee696a7403a713c3
Brandon Nay <brandonnay21@gmail.com>
2020-02-04 16:38:51 -0700

    Remove unused import

--- Taproot-backend/taproot/taproot/urls.py
+++ Taproot-backend/taproot/taproot/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
-from rest_framework import routers
 
 urlpatterns = [
     path('admin/', admin.site.urls),
