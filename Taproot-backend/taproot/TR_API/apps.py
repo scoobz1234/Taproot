4665cc5312623fa8b240cd64c9c143f994ea8a37
Brandon Nay <brandonnay21@gmail.com>
2020-02-04 16:35:55 -0700

    Create API directory

---
+++ Taproot-backend/taproot/TR_API/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TrApiConfig(AppConfig):
+    name = 'TR_API'
---
+++ Taproot-backend/taproot/TR_API/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from django.urls import include
+from TR_API import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
