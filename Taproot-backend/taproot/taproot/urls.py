485473cd745c34f746a42f5f56513b96768e2be3
Brandon Nay <brandonnay21@gmail.com>
2020-02-04 16:35:39 -0700

    Update urls.py

--- Taproot-backend/taproot/taproot/urls.py
+++ Taproot-backend/taproot/taproot/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from rest_framework import routers
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('API/', include('TR_API.urls')),
 ]
