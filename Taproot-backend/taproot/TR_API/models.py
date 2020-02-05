7af27ea7a67ad39c4f5e8d0242ef7dd670636a67
Brandon Nay <brandonnay21@gmail.com>
2020-02-04 17:12:13 -0700

    Add first few tables
    Tentative. intervention, behavior, encounter, feedback, feedback category

--- Taproot-backend/taproot/TR_API/models.py
+++ Taproot-backend/taproot/TR_API/models.py
@@ -1,3 +1,144 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# class Example(models.Model):
+#     column_name = models.CharField(max_length=10)
+
+#     def __eq__(self, other):
+#         return self.pk == other
+
+#     def __ne__(self, other):
+#         return self.pk != other
+
+#     def __gt__(self, other):
+#         return self.pk > other
+
+#     def __lt__(self, other):
+#         return self.pk < other
+
+#     def __hash__(self):
+#         return hash(self.pk)
+
+#     class Meta:
+#         db_table = "TBL_EXAMPLE"
+
+class Behavior(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.PROTECT)
+    name = models.CharField(max_length=50)
+    info = models.TextField()
+
+    def __eq__(self, other):
+        return self.pk == other
+
+    def __ne__(self, other):
+        return self.pk != other
+
+    def __gt__(self, other):
+        return self.pk > other
+
+    def __lt__(self, other):
+        return self.pk < other
+
+    def __hash__(self):
+        return hash(self.pk)
+
+    class Meta:
+        db_table = "TBL_BEHAVIOR"
+
+
+class Encounter(models.Model):
+    event = models.AutoField(primary_key=True)
+    patient = models.ForeignKey(User, on_delete=models.PROTECT)
+    caregiver = models.ForeignKey(User, on_delete=models.PROTECT)
+    behavior = models.ForeignKey('Behavior', on_delete=models.PROTECT)
+    intervention = models.ForeignKey('Intervention', on_delete=models.PROTECT)
+    date = models.DateTimeField(auto_now_add=True)
+    outcome = models.BooleanField() # TODO: pass/fail? or need a third option?
+    notes = models.TextField()
+
+    def __eq__(self, other):
+        return self.pk == other
+
+    def __ne__(self, other):
+        return self.pk != other
+
+    def __gt__(self, other):
+        return self.pk > other
+
+    def __lt__(self, other):
+        return self.pk < other
+
+    def __hash__(self):
+        return hash(self.pk)
+
+    class Meta:
+        db_table = "TBL_ENCOUNTER"
+
+
+class Feedback(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.PROTECT)
+    category = models.ForeignKey('FeedbackCategory', on_delete=models.PROTECT)
+    date = models.DateTimeField(auto_now_add=True)
+    text = models.TextField()
+
+    def __eq__(self, other):
+        return self.pk == other
+
+    def __ne__(self, other):
+        return self.pk != other
+
+    def __gt__(self, other):
+        return self.pk > other
+
+    def __lt__(self, other):
+        return self.pk < other
+
+    def __hash__(self):
+        return hash(self.pk)
+
+    class Meta:
+        db_table = "TBL_FEEDBACK"
+
+
+class FeedbackCategory(models.Model):
+    category = models.CharField(max_length=30)
+
+    def __eq__(self, other):
+        return self.pk == other
+
+    def __ne__(self, other):
+        return self.pk != other
+
+    def __gt__(self, other):
+        return self.pk > other
+
+    def __lt__(self, other):
+        return self.pk < other
+
+    def __hash__(self):
+        return hash(self.pk)
+
+    class Meta:
+        db_table = "TBL_FEEDBACK_CATEGORY"
+
+
+class Intervention(models.Model):
+    category = models.CharField(max_length=30)
+
+    def __eq__(self, other):
+        return self.pk == other
+
+    def __ne__(self, other):
+        return self.pk != other
+
+    def __gt__(self, other):
+        return self.pk > other
+
+    def __lt__(self, other):
+        return self.pk < other
+
+    def __hash__(self):
+        return hash(self.pk)
+
+    class Meta:
+        db_table = "TBL_INTERVENTION"
