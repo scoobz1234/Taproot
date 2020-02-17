56f3211b679c2090479bec3b111cb562b4a7d57f
Brandon Nay <brandonnay21@gmail.com>
2020-02-17 00:07:16 -0700

    Add caregiver, facility, resident and intervention models

--- Taproot-backend/taproot/TR_API/models.py
+++ Taproot-backend/taproot/TR_API/models.py
@@ -23,7 +23,6 @@ from django.contrib.auth.models import User
 #         db_table = "TBL_EXAMPLE"
 
 class Behavior(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.PROTECT)
     name = models.CharField(max_length=50)
     info = models.TextField()
 
@@ -46,14 +45,50 @@ class Behavior(models.Model):
         db_table = "TBL_BEHAVIOR"
 
 
+class Caregiver(models.Model):
+    GENDER = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    facility = models.ForeignKey('Facility', on_delete=models.PROTECT)  # TODO: behavior, privacy
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    dob = models.DateField()
+    gender = models.CharField(max_length=1, choices=GENDER)
+    email = models.EmailField()
+    phone = models.IntegerField()
+    active = models.BooleanField(default=True)
+
+    def __eq__(self, other):
+        return self.pk == other
+
+    def __ne__(self, other):
+        return self.pk != other
+
+    def __gt__(self, other):
+        return self.pk > other
+
+    def __lt__(self, other):
+        return self.pk < other
+
+    def __hash__(self):
+        return hash(self.pk)
+
+    class Meta:
+        db_table = "TBL_CAREGIVER"
+
+
 class Encounter(models.Model):
     event = models.AutoField(primary_key=True)
-    patient = models.ForeignKey(User, on_delete=models.PROTECT)
+    resident = models.ForeignKey('Resident', on_delete=models.PROTECT, null=True)
     caregiver = models.ForeignKey(User, on_delete=models.PROTECT)
     behavior = models.ForeignKey('Behavior', on_delete=models.PROTECT)
     intervention = models.ForeignKey('Intervention', on_delete=models.PROTECT)
     date = models.DateTimeField(auto_now_add=True)
     outcome = models.BooleanField() # TODO: pass/fail? or need a third option?
+    behavior_rating = models.IntegerField()
     notes = models.TextField()
 
     def __eq__(self, other):
@@ -73,6 +108,34 @@ class Encounter(models.Model):
 
     class Meta:
         db_table = "TBL_ENCOUNTER"
+
+
+class Facility(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    address_ext = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    zipcode = models.IntegerField()
+    phone = models.IntegerField()
+
+    def __eq__(self, other):
+        return self.pk == other
+
+    def __ne__(self, other):
+        return self.pk != other
+
+    def __gt__(self, other):
+        return self.pk > other
+
+    def __lt__(self, other):
+        return self.pk < other
+
+    def __hash__(self):
+        return hash(self.pk)
+
+    class Meta:
+        db_table = "TBL_FACILITY"
 
 
 class Feedback(models.Model):
@@ -123,7 +186,8 @@ class FeedbackCategory(models.Model):
 
 
 class Intervention(models.Model):
-    category = models.CharField(max_length=30)
+    name = models.CharField(max_length=50)
+    info = models.TextField()
 
     def __eq__(self, other):
         return self.pk == other
@@ -142,3 +206,35 @@ class Intervention(models.Model):
 
     class Meta:
         db_table = "TBL_INTERVENTION"
+
+
+class Resident(models.Model):   # TODO
+    GENDER = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+    ]
+
+    facility = models.ForeignKey('Facility', on_delete=models.PROTECT)  # TODO: behavior, privacy
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    dob = models.DateField()
+    gender = models.CharField(max_length=1, choices=GENDER)
+    active = models.BooleanField(default=True)
+
+    def __eq__(self, other):
+        return self.pk == other
+
+    def __ne__(self, other):
+        return self.pk != other
+
+    def __gt__(self, other):
+        return self.pk > other
+
+    def __lt__(self, other):
+        return self.pk < other
+
+    def __hash__(self):
+        return hash(self.pk)
+
+    class Meta:
+        db_table = "TBL_RESIDENT"
