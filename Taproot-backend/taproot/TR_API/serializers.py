5be5023237e38d28f7a2d8d626c84f61b1e57b61
Brandon Nay <brandonnay21@gmail.com>
2020-02-26 01:10:05 -0700

    Add 5 API endpoints
    behaviors, interventions, encunters, caregivers, facilities

--- Taproot-backend/taproot/TR_API/serializers.py
+++ Taproot-backend/taproot/TR_API/serializers.py
@@ -1,14 +1,52 @@
-from django.contrib.auth.models import User, Group
+# from django.contrib.auth.models import User, Group
 from rest_framework import serializers
+from .models import Behavior
+from .models import Caregiver
+from .models import Encounter
+from .models import Facility
+from .models import Intervention
+# from .models import *
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+# class GroupSerializer(serializers.HyperlinkedModelSerializer):
+#     class Meta:
+#         model = Group
+#         fields = ['id', 'url', 'name']
+
+
+# class UserSerializer(serializers.HyperlinkedModelSerializer):
+#     groups = GroupSerializer(many=True, read_only=True)
+#     class Meta:
+#         model = User
+#         fields = ['id', 'url', 'username', 'password', 'groups']
+
+
+class InterventionSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = User
-        fields = ['id', 'url', 'username', 'password', 'groups']
+        model = Intervention
+        fields = ['id', 'name', 'info']
 
 
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
+class BehaviorSerializer(serializers.HyperlinkedModelSerializer):
+    interventions = InterventionSerializer(many=True, read_only=True)
     class Meta:
-        model = Group
-        fields = ['id', 'url', 'name']
+        model = Behavior
+        fields = ['id', 'name', 'info', 'interventions']
+
+
+class CaregiverSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Caregiver
+        fields = ['id', 'name', 'info']
+
+
+class EncounterSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Encounter
+        fields = ['id', 'name', 'info']
+
+
+class FacilitySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Facility
+        fields = ['id', 'name', 'info']
--- Taproot-backend/taproot/TR_API/urls.py
+++ Taproot-backend/taproot/TR_API/urls.py
@@ -4,8 +4,13 @@ from TR_API import views
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+# router.register(r'users', views.UserViewSet)
+# router.register(r'groups', views.GroupViewSet)
+router.register(r'behaviors', views.BehaviorViewSet)
+router.register(r'caregivers', views.CaregiverViewSet)
+router.register(r'encounters', views.EncounterViewSet)
+router.register(r'facilities', views.FacilityViewSet)
+router.register(r'interventions', views.InterventionViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
