cf2765c35b1666055eb64281b5837c62955c2503
Brandon Nay <brandonnay21@gmail.com>
2020-02-26 00:06:20 -0700

    Add middleware for serving static files

--- Taproot-backend/taproot/taproot/settings/settings.py
+++ Taproot-backend/taproot/taproot/settings/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = BASE_APPS + ADDED_APPS
 
 BASE_MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
